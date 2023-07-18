32af9a510e8e4c950a8eef9b3a9c9fb8556f8ad5
HenryNguyen <henrynguyen@theitservice.com.au>
2023-07-18 21:00:14 +1000

    Add watermachine to the logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {createMachine} from "xstate";
+import {useMachine} from "@xstate/react";
 
+const waterMachine = createMachine({
+  id: 'water',
+  initial: 'liquid',
+  states: {
+    ice:  {
+      on : {
+        HEAT: {
+          target: "liquid"
+        }
+
+      }
+    },
+    liquid: {
+      on: {
+        HEAT: {
+          target: "gas"
+        },
+        FREEZE: {
+          target: "ice"
+        }
+      }
+    },
+    gas: {
+      on: {
+        HEAT: {
+          target: "plasma"
+        },
+        FREEZE: {
+          target: "liquid"
+        }
+      }
+    },
+    plasma: {
+      on: {
+        FREEZE: {
+          target: "gas"
+        }
+      }
+    }
+  }
+});
 function App() {
+  const [current, send] = useMachine(waterMachine);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <strong>{current.value}</strong>
+        <br />
+
+        <button onClick={() => send("HEAT")}>heat</button>
+        <button onClick={() => send("FREEZE")}>freeze</button>
+
+      </div>
   );
 }
 
